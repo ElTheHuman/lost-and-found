749a12d15866c9364357cc5ce9b728db9d39e7ba
ElTheHuman <felixcristiano102938@gmail.com>
2024-10-10 22:42:24 +0700

    Fix deployment issue

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './main.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/LostAndFound/'>
+    <BrowserRouter basename='/lost-and-found/'>
       <App />
     </BrowserRouter>
   </StrictMode>
