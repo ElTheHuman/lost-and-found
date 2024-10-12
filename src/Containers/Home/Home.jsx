b00f29e7f1642e8c26b4801d8f705839749b4dd2
ElTheHuman <felixcristiano102938@gmail.com>
2024-10-12 17:08:07 +0700

    Add location in Home

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -28,6 +28,45 @@ const Home = () => {
                         {/* Empty */}
                     </div>
                 </div>
+                <div className="Home__all-location--container">
+                    <div className="Home__all-location">
+                        <div className="Home__time-location">
+                            <div className="Home__date--container">
+                                <div className="Home__day">
+                                    <p className="Home__day--text">Jumat</p>
+                                </div>
+                                <div className="Home__date">
+                                    <p className="Home__date--text">11 Oktober 2024</p>
+                                </div>
+                            </div>
+                            <div className="Home__location-current">
+                                <p className="Home__location-current--title">Now you're here</p>
+                                <p className="Home__location-current--desc">BINUS</p>
+                                <p className="Home__location-current--desc">Greater Jakarta</p>
+                            </div>
+                        </div>
+                        <div className="Home__location-suggestion--container">
+                            <div className="Home__location-suggestion" id="Home__location-suggestion--left">
+                                <p className="Home__location-suggestion--subtitle">Lost & Bid</p>
+                                <p className="Home__location-suggestion--title">BINUS Anggrek</p>
+                                <p className="Home__location-suggestion--detail">Detailed Loc 123</p>
+                                <p className="Home__location-suggestion--desc">desc</p>
+                            </div>
+                            <div className="Home__location-suggestion" id="Home__location-suggestion--center">
+                                <p className="Home__location-suggestion--subtitle">Lost & Bid</p>
+                                <p className="Home__location-suggestion--title">BINUS Anggrek</p>
+                                <p className="Home__location-suggestion--detail">Detailed Loc 123</p>
+                                <p className="Home__location-suggestion--desc">desc</p>
+                            </div>
+                            <div className="Home__location-suggestion" id="Home__location-suggestion--right">
+                                <p className="Home__location-suggestion--subtitle">Lost & Bid</p>
+                                <p className="Home__location-suggestion--title">BINUS Anggrek</p>
+                                <p className="Home__location-suggestion--detail">Detailed Loc 123</p>
+                                <p className="Home__location-suggestion--desc">desc</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div className="Home__all-content">
                     <div className="Home__title">
                         Found Update
