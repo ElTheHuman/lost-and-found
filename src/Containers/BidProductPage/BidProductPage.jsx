0815bb28b13b5d8ae1a1d6a99a4e898f26f9a609
ElTheHuman <felixcristiano102938@gmail.com>
2024-12-19 02:28:44 +0700

    Add bid product page UI

--- src/Containers/BidProductPage/BidProductPage.jsx
+++ src/Containers/BidProductPage/BidProductPage.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import './BidProductPage.css';
 import BidProductPageSidebar from "./BidProductPageSidebar/BidProductPageSidebar";
+import BidProductPageMainContent from "./BidProductPageMainContent/BidProductPageMainContent";
 
 const BidProductPage = ({children}) => {
 
     return (
         <div className="BidProductPage">
             <BidProductPageSidebar />
-        </div>   
+            <BidProductPageMainContent />
+        </div>
     )
 
 }
